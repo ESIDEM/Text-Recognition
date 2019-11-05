8b8b10bddb81bc4e1daca182b4f397cbd7337b3b
Sam Esidem <samesidem@gmail.com>
2019-11-05 16:38:39 +0100

    Feat: Developer details added

---
+++ app/src/main/java/ng/com/techdepo/textrecognition/DeveloperProfile.kt
@@ -0,0 +1,14 @@
+package ng.com.techdepo.textrecognition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeveloperProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_page)
+        supportActionBar!!.title = "Developer's Profile"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+}
--- app/src/main/java/ng/com/techdepo/textrecognition/MainActivity.kt
+++ app/src/main/java/ng/com/techdepo/textrecognition/MainActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.ActivityNotFoundException
+import android.net.Uri
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,22 +18,51 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar!!.title="Scan! Copy! Recognize"
 
-        text_recognition.setOnClickListener {
+        copy.setOnClickListener {
             startActivity(Intent(this,TextRecognition::class.java))
         }
 
-        bar_code.setOnClickListener {
+        scan.setOnClickListener {
 
             startActivity(Intent(this, BarCodeScaner::class.java))
         }
 
-        image_recognition.setOnClickListener {
+        recognize.setOnClickListener {
             startActivity(Intent(this, ImageRecognition::class.java))
+        }
+
+        rate.setOnClickListener {
+            rateApp()
+        }
+
+        dev.setOnClickListener {
+
+            startActivity(Intent(this,DeveloperProfile::class.java))
         }
 
     }
 
 
+ fun rateApp() {
 
+val uri = Uri.parse("market://details?id=" + this.packageName)
+val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+ // To count with Play market backstack, After pressing back button,
+        // to taken back to our application, we need to add following flags to intent.
+        goToMarket.addFlags(
+            Intent.FLAG_ACTIVITY_NO_HISTORY or
+    Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+    Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+        )
+try
+{
+startActivity(goToMarket)
+}
+catch (e:ActivityNotFoundException) {
+startActivity(Intent(Intent.ACTION_VIEW,
+Uri.parse("http://play.google.com/store/apps/details?id=" + this.packageName)))
+}
+
+}
 
 }
